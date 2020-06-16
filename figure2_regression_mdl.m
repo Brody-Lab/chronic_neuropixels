9e4c85eef9febaf31c16475c3cef32d1c70b2463
thomaszhihaoluo <thomas.zhihao.luo@gmail.com>
2020-06-16 13:40:24 -0400

    figure2.m nows calls the code to make panels M, N, O, and P.

--- figure2_regression_mdl.m
+++ figure2_regression_mdl.m
@@ -1,7 +1,15 @@
-% Make panels N and O of figure 2
+% Make panels M,N,O,P of figure 2
 
-if ~exist('S', 'var')
-    P=get_parameters;
-    load(P.sum_exp_trodes.data_path);
+add_folders_to_path
+
+from_scratch = false;
+if from_scratch
+    sum_exp_trodes_run_mdl
+else
+    if ~exist('S', 'var')
+        P = get_parameters;
+        load(P.sum_exp_trodes.data_path);
+    end
 end
+
 sum_exp_trodes_plot_coef(S, 'mdl_inds', 1:5)
